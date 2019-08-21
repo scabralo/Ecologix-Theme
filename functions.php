9e7b8eedb1256f9794f00d4e21c436ed518467e4
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-21 15:40:09 -0400

    Adds secondary menu to functions.php file

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ if ( ! function_exists( 'ecologix_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'ecologix' ),
+			'menu-2' => esc_html__( 'Secondary', 'ecologix' ),
 		) );
 
 		/*
