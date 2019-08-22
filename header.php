c8b589e6a71f0b0905dc376e5a1cc4e7b4f0ee76
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-22 10:26:54 -0400

    Updates order of elements within Header.php

--- header.php
+++ header.php
@@ -25,26 +25,6 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ecologix' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_header_image_tag(); ?></a>
-			<?php
-			// the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$ecologix_description = get_bloginfo( 'description', 'display' );
-			if ( $ecologix_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $ecologix_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ecologix' ); ?></button>
 			<?php
@@ -54,16 +34,36 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
-		<nav id="secondary-navigation" class="main-navigation secondary-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ecologix' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-2',
-				'menu_id'        => 'secondary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-
+		<div class="eco-wrapper">
+			<div class="site-branding">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_header_image_tag(); ?></a>
+				<?php
+				// the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$ecologix_description = get_bloginfo( 'description', 'display' );
+				if ( $ecologix_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $ecologix_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+			<nav id="secondary-navigation" class="main-navigation secondary-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ecologix' ); ?></button>
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-2',
+					'menu_id'        => 'secondary-menu',
+				) );
+				?>
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
