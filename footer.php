4db875c54dbacb396a2a53b1621085f15999c2e5
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-23 14:44:39 -0400

    Updates footer to show logo from widget and not hardcoded

--- footer.php
+++ footer.php
@@ -14,31 +14,31 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="eco-upper-footer">
-			<div class="eco-logo-wrapper">
-				<img src="/ecologix/wp-content/uploads/2019/08/ecologix-logo-white.png" alt="Ecologix Logo for dark backgrounds">
-			</div>
+		<div class="eco-upper-footer eco-wrapper">
+			<?php if ( is_active_sidebar( 'footer-logo-1' ) ) dynamic_sidebar('footer-logo-1'); ?>
 			<div class="eco-widgets-wrapper">
 				<?php 
-					if ( ! is_active_sidebar( 'footer-1' ) ) dynamic_sidebar('footer-1');
-					if ( ! is_active_sidebar( 'footer-2' ) ) dynamic_sidebar('footer-2');
-					if ( ! is_active_sidebar( 'footer-3' ) ) dynamic_sidebar('footer-3');
-					if ( ! is_active_sidebar( 'footer-4' ) ) dynamic_sidebar('footer-4');
+					if ( is_active_sidebar( 'footer-1' ) ) dynamic_sidebar('footer-1');
+					if ( is_active_sidebar( 'footer-2' ) ) dynamic_sidebar('footer-2');
+					if ( is_active_sidebar( 'footer-3' ) ) dynamic_sidebar('footer-3');
+					if ( is_active_sidebar( 'footer-4' ) ) dynamic_sidebar('footer-4');
 				?>
 			</div>
 		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ecologix' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ecologix' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ecologix' ), 'ecologix', '<a href="http://www.sergiocabral.io">Sergio Cabral</a>' );
-				?>
+			<div class="eco-wrapper">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ecologix' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'ecologix' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'ecologix' ), 'ecologix', '<a href="http://www.sergiocabral.io">Sergio Cabral</a>' );
+					?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
