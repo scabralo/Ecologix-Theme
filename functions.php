89c76b0695e639e996fffe10ee88eab5ebd332af
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-23 14:44:04 -0400

    Adds footer logo as a widgetized area

--- functions.php
+++ functions.php
@@ -115,6 +115,15 @@ function ecologix_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Logo', 'ecologix' ),
+		'id'            => 'footer-logo-1',
+		'description'   => esc_html__( 'Add widgets here.', 'ecologix' ),
+		'before_widget' => '<div id="%1$s" class="widget eco-logo-wrapper %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
 		'name'          => esc_html__( 'Footer 1', 'ecologix' ),
 		'id'            => 'footer-1',
 		'description'   => esc_html__( 'Add widgets here.', 'ecologix' ),
