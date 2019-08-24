e7d26bf5aacdf5c6a48cf3cf50e426fe1cf8c741
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-24 07:28:39 -0400

    Eliminates WordPress loop from custom template

--- custom-templates/homepage-template.php
+++ custom-templates/homepage-template.php
@@ -11,23 +11,11 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<article class="page type-page status-publish hentry">
+				<div class="entry-content">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+				</div>
+			</article>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
