b4cff59cd18d4bccc339b22f4a0ad8f75674ee75
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-26 13:55:19 -0400

    Disables Sidebar's registration on functions.php

--- functions.php
+++ functions.php
@@ -105,15 +105,15 @@ add_action( 'after_setup_theme', 'ecologix_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function ecologix_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'ecologix' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'ecologix' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+	// register_sidebar( array(
+	// 	'name'          => esc_html__( 'Sidebar', 'ecologix' ),
+	// 	'id'            => 'sidebar-1',
+	// 	'description'   => esc_html__( 'Add widgets here.', 'ecologix' ),
+	// 	'before_widget' => '<section id="%1$s" class="widget %2$s">',
+	// 	'after_widget'  => '</section>',
+	// 	'before_title'  => '<h2 class="widget-title">',
+	// 	'after_title'   => '</h2>',
+	// ) );
 	register_sidebar( array(
 		'name'          => esc_html__( 'Footer Logo', 'ecologix' ),
 		'id'            => 'footer-logo-1',
